3ad9802c240a069b836f2137a3c10e6a13cfb5f6
Maroua Cherki <cherkimaroua7@gmail.com>
2025-11-19 11:50:58 +0100

    Add files via upload

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Counter from './statecomp/Counter';
+import MoodBoard from './statecomp/MoodBoard';
 function App() {
   return (
     <div>
       <Counter />
-      
+        <MoodBoard/>
+
     </div>
   );
 
